53454bb416e3111e133ec73c5eecefa4aec024e7
rhandell-dev <ababa.rhandell@gmail.com>
2022-12-28 13:33:10 -0700

    add in memory authentication config

---
+++ src/main/java/com/example/takehome/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.takehome.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder().username("user")
+                .password(passwordEncoder().encode("user")).roles("USER")
+                .build();
+        UserDetails admin = User.builder().username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER", "ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.anonymous().principal("guest").authorities("ROLE_GUEST").and()
+                .authorizeHttpRequests().requestMatchers("/continent/get")
+                .hasAnyAuthority("ROLE_GUEST", "ROLE_USER").and().httpBasic();
+        return http.build();
+    }
+
+}
